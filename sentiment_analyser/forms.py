02d0fb5ed47a8a487bdd13e5efc1cf9f5fe1549a
deeptika <deeptika24@gmail.com>
2020-07-05 14:13:53 +0530

    Changed type of contact number field

--- sentiment_analyser/forms.py
+++ sentiment_analyser/forms.py
@@ -5,6 +5,7 @@ from .models import User
 class UserForm(forms.ModelForm): 
     last_name = forms.CharField(required=False)
     email = forms.EmailField(required=True)
+    contact_number = forms.RegexField(regex=r'^[6-9]\d{9}$')
     class Meta: 
         model = User
         fields = "__all__"
