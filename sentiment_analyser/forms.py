a2bcb2f8b9361205e42f6990cda6144de8de3751
deeptika <deeptika24@gmail.com>
2020-07-05 13:57:07 +0530

    Rendered form using django-crispy

--- sentiment_analyser/forms.py
+++ sentiment_analyser/forms.py
@@ -3,7 +3,8 @@ from django import forms
 from .models import User
 
 class UserForm(forms.ModelForm): 
+    last_name = forms.CharField(required=False)
+    email = forms.EmailField(required=True)
     class Meta: 
         model = User
-        #fields = "__all__"
-        exclude = ['ext.']
+        fields = "__all__"
