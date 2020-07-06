f7349d5f54f7a4f578d09f165959bd0c0ba33df8
deeptika <deeptika24@gmail.com>
2020-07-06 11:06:02 +0530

    Updated crispy form with form helpers

--- sentiment_analyser/forms.py
+++ sentiment_analyser/forms.py
@@ -2,6 +2,7 @@
 from django import forms 
 from .models import User
 
+'''
 class UserForm(forms.ModelForm): 
     last_name = forms.CharField(required=False)
     email = forms.EmailField(required=True)
@@ -9,3 +10,26 @@ class UserForm(forms.ModelForm):
     class Meta: 
         model = User
         fields = ['first_name', 'last_name', 'email', 'contact_number']
+'''
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+class UserForm(forms.ModelForm):
+    last_name = forms.CharField(required=False)
+    email = forms.EmailField(required=True)
+    contact_number = forms.RegexField(regex=r'^[6-9]\d{9}$')
+
+    class Meta: 
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'contact_number']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-UserForm'
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        #self.helper.form_action = 'action_page.php'
+        self.helper.add_input(Submit('submit', 'Submit'))
+        self.helper.add_input(Submit('cancel', 'Cancel'))
