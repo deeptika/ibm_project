ac4afe24a8da8b0b5b444e66a3b34aa4ce47edf4
deeptika <deeptika24@gmail.com>
2020-07-06 15:44:01 +0530

    Added form saving ability and alert for registering successfully

--- sentiment_analyser/views.py
+++ sentiment_analyser/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import UserForm
 
 # Create your views here.
@@ -7,7 +8,14 @@ def home(request):
     return render(request,'sentiment_analyser/home.html')
 
 def form(request):
-    user = UserForm()
-    return render(request, 'sentiment_analyser/form.html', {'form':user})
- 
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Thanks for registering!')
+            return redirect('home')
+    else:
+        form = UserForm()
+    return render(request, 'sentiment_analyser/form.html', {'form':form})
+             
 
